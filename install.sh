e2828be9d4513cfc0262cfbef7d2618b28136540
inaz2 <inaz2@twitter>
2013-01-02 20:41:00 +0900

    install.sh: fix to execute from any directory

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 shopt -s extglob
-for i in !($0|README); do
+
+script_path="$PWD/$0"
+script_dir="${script_path%/*}"
+script_name="${script_path##*/}"
+
+cd "$script_dir"
+for i in !($script_name|README); do
     ln -s "$PWD/$i" "$HOME/.$i"
 done
