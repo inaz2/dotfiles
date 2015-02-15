cce0c2e5b50d191b3131491e1bd35cc9a2865d63
inaz2 <inaz2@twitter>
2015-02-16 00:07:33 +0900

    install.sh: use "ln -i"

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-script_path="$PWD/$0"
+script_path=$(readlink -e "$0")
 script_dir="${script_path%/*}"
 script_name="${script_path##*/}"
 
@@ -10,9 +10,7 @@ for i in *; do
     $script_name|README)
         ;;
     *)
-        if [[ $(readlink -e "$HOME/.$i") != "$PWD/$i" ]]; then
-            ln -s "$PWD/$i" "$HOME/.$i"
-        fi
+        ln -i -s "$PWD/$i" "$HOME/.$i"
         ;;
     esac
 done
